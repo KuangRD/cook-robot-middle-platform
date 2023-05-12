76e788be22ba87e5d6343eeacfd27e4263aea347
kanyuanzhi <kan@mail.ustc.edu.cn>
2023-05-12 15:16:18 +0800

    add init.sh

--- init.sh
+++ init.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # 添加第一个脚本
-lxterminal -e "/home/xupc/Desktop/init_controller.sh"
+lxterminal --command="/home/xupc/Desktop/init_controller.sh" &
 
 # 添加第二个脚本
-lxterminal -e "/home/xupc/Desktop/init_server.sh"
+lxterminal --command="/home/xupc/Desktop/init_server.sh" &
 
 # 添加第三个脚本
-lxterminal -e "/home/xupc/Desktop/init_ui_new.sh"
+lxterminal --command="/home/xupc/Desktop/init_ui_new.sh" &
