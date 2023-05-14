6af0a152e25df62ff73be372a58deccca4f7f3ac
kanyuanzhi <kan@mail.ustc.edu.cn>
2023-05-15 00:37:30 +0800

    update

--- main.py
+++ main.py
@@ -77,8 +77,10 @@ if __name__ == "__main__":
 
     if sys.platform == "linux":
         # _host = "169.254.216.10"
-        _host = "127.0.0.1"
+        # _host = "127.0.0.1"
+        _host = "0.0.0.0"
     else:
         # _host = "169.254.70.55"
-        _host = "127.0.0.1"
+        # _host = "127.0.0.1"
+        _host = "0.0.0.0"
     app.run(host=_host, port=8888, debug=False)
